765a7fc884a6054a2277c78a41a899a03885ddfc
pamal pahasara <pamalpahasara@gmail.com>
2025-06-08 12:35:32 +0530

    create frame and panel structure

---
+++ gamePanel.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class gamePanel extends JPanel implements ActionListener {
+
+    gamePanel(){
+    }
+
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class myKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+}
